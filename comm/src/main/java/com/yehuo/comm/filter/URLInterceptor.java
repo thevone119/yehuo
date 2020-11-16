0f50c0738db3a6504ab278d84a423a8932625642
thevone119 <57717235@qq.com>
2020-11-16 12:06:05 +0800

    123

---
+++ comm/src/main/java/com/yehuo/comm/filter/URLInterceptor.java
@@ -0,0 +1,182 @@
+package com.yehuo.comm.filter;
+
+
+import com.alibaba.fastjson.JSON;
+import com.bingo.common.model.SessionUser;
+import com.bingo.common.service.SessionCacheService;
+import com.bingo.common.utility.XJsonInfo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+
+/**
+ * 所有请求的拦截器
+ * Created by Administrator on 2018-06-26.
+ */
+@Component
+public class URLInterceptor extends HandlerInterceptorAdapter {
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+    private long currTime = 0;
+
+    private static int currThread = 0;
+    private static Map<String, String> currThreadMap = new HashMap<String, String>();
+    private static final int maxThread = 50;//最大允许并发线程数
+
+
+    @Resource
+    private SessionCacheService sessionCache;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        currTime = System.currentTimeMillis();
+        HandlerMethod hm = (HandlerMethod) handler;
+        if (isbusy()) {//系统繁忙，终止部分请求
+            if (XJsonInfo.class.getName().equals(hm.getMethod().getReturnType().getName())) {
+                response.setHeader("Content-type", "text/html;charset=UTF-8");
+                String retstr = JSON.toJSONString(new XJsonInfo(false, "对不起，系统繁忙，请稍候再试.错误码0021"));
+                response.getOutputStream().write(retstr.getBytes("utf-8"));
+            } else {
+                response.sendRedirect("/error/error.html");
+            }
+            return false;
+        }
+        boolean ret = preHandleProxy(request, response, handler);
+
+        String actionname = hm.getMethod().toString();
+        if (ret) {
+            threadadd(actionname + currTime);
+        } else {
+            if (XJsonInfo.class.getName().equals(hm.getMethod().getReturnType().getName())) {
+                response.setHeader("Content-type", "text/html;charset=UTF-8");
+                String retstr = JSON.toJSONString(new XJsonInfo(false, "对不起，您还没有登录，请登录后再进行操作."));
+                response.getOutputStream().write(retstr.getBytes("utf-8"));
+            } else {
+                //直接跳转到首页
+                response.sendRedirect("/");
+            }
+        }
+        return ret;
+    }
+
+    /**
+     * 前置拦截处理
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    private boolean preHandleProxy(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HandlerMethod hm = (HandlerMethod) handler;
+        String actionname = hm.getMethod().getDeclaringClass().getName() + "." + hm.getMethod().getName().toString();
+        //1.获取方法的注解
+        ControllerFilter filter = hm.getMethodAnnotation(ControllerFilter.class);
+        //2.如果方法上没有注解，则获取类上的注解
+        if (filter == null) {
+            filter = hm.getBean().getClass().getAnnotation(ControllerFilter.class);
+        }
+        //3.如果方法和类上都没有注解，则直接返回
+        if (filter == null) {
+            return true;
+        }
+        //4.获取登录用户
+        SessionUser user = (SessionUser) sessionCache.getLoginUser();
+
+        //4.判断注解权限，判断是否登录
+        if (filter.LoginType() == ControllerType.LOGIN_TYPE_LOGIN) {
+            if (user == null) {
+                logger.info("当前用户没有权限访问此页面01" + actionname);
+                return false;
+            }
+        }
+        //4.判断注解权限，判断是否管理员
+        if (filter.UserType() == ControllerType.USER_TYPE_ADMIN) {
+            if (user == null || user.getUsertype() != 1) {
+                logger.info("当前用户没有权限访问此页面02" + actionname);
+                return false;
+            }
+        }
+        //4.判断注解权限，判断角色
+        if (filter.roles() != null && filter.roles().length > 0) {
+            if (user == null) {
+                logger.info("当前用户没有权限访问此页面031" + actionname);
+                return false;
+            }
+            boolean hasrole = false;
+            for (String role : filter.roles()) {
+                for (String urole : user.getRolecodes()) {
+                    if (role.equals(urole)) {
+                        hasrole = true;
+                        break;
+                    }
+                }
+            }
+            if (!hasrole) {
+                logger.info("当前用户没有权限访问此页面032" + actionname);
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    private static synchronized void threadadd(String key) {
+        currThread++;
+        currThreadMap.put(key, null);
+    }
+
+    private static synchronized void threaddel(String key) {
+        currThread--;
+        currThreadMap.remove(key);
+    }
+
+    private boolean isbusy() {
+        if (currThread >= maxThread) {
+            logger.info("当前并发线程数(锁):" + currThread + currThreadMap.toString());
+            return true;
+        }
+        logger.info("当前并发线程数:" + currThread);
+        return false;
+    }
+
+    //返回处理
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        //String param=request.getParameter(METHOD);
+        HandlerMethod hm = (HandlerMethod) handler;
+
+        String actionname = hm.getMethod().getDeclaringClass().getName() + "." + hm.getMethod().getName().toString();
+        threaddel(actionname + currTime);
+        //Runtime imp = Runtime.getRuntime();
+        //String memory = ",maxMemory:"+imp.maxMemory()/1024/1024+"M,freeMemory:"+imp.freeMemory()/1024/1024+"M"+",userMemory:"+(imp.maxMemory()-imp.freeMemory())/1024/1024+"M";
+        String memory = "";
+        logger.info("Action end:" + actionname + ",usetime:" + (System.currentTimeMillis() - currTime) + memory);
+    }
+
+
+    /**
+     * 对字符串当中的JS代码 进行过滤,全部替换为"非法字符"
+     *
+     * @param str 要过滤的字符串
+     * @return 过滤后的字符串
+     */
+    public static String doScriptFilter(String str) {
+        str = Pattern.compile("<script.*?>.*?</script>", Pattern.CASE_INSENSITIVE).matcher(str).replaceAll("****");
+        return str;
+    }
+}
---
+++ comm/src/main/java/com/yehuo/comm/utility/QRCodeUtils.java
@@ -0,0 +1,195 @@
+package com.yehuo.comm.utility;
+
+import com.bingo.common.http.HttpReturn;
+import com.bingo.common.http.MyOkHttp;
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import jp.sourceforge.qrcode.QRCodeDecoder;
+import jp.sourceforge.qrcode.data.QRCodeImage;
+import org.json.JSONObject;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2018-07-20.
+ * 创建和读取二维码
+ */
+public class QRCodeUtils {
+    public static final int WIDTH = 300;
+    public static final int HEIGHT = 300;
+    public static final String FORMAT = "png";
+    public static final String CHARTSET = "utf-8";
+
+    public static void main(String[] args) {
+        String filePath = "C:\\Users\\Administrator\\Desktop\\wx\\QQ图片20180830180859.jpg";
+        //createQRcode(filePath);
+        testReadQRcode(filePath);
+    }
+
+    private static void testReadQRcode(String filePath) {
+        Result result = getQRresult(filePath);
+        if (result != null) {
+            System.out.println("二维码内容：" + result.getText());
+            System.out.println("二维码格式：" + result.getBarcodeFormat());
+        }
+    }
+
+    /**
+     *
+     * @Title:createQRcode
+     * @Description:创建二维码
+     * @param contents
+     * @author doubledumbao
+     * @修改时间：2018年2月26日 上午9:44:45
+     * @修改内容：创建
+     */
+    public static void createQRcode(String contents,OutputStream out) {
+        /**
+         * 如果用的jdk是1.9，需要配置下面这一行。
+         */
+        //System.setProperty("java.specification.version", "1.9");
+        HashMap<EncodeHintType, Object> hints = new HashMap<>();
+        hints.put(EncodeHintType.CHARACTER_SET, CHARTSET);
+        hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.M);
+        hints.put(EncodeHintType.MARGIN, 2);
+        try {
+            BitMatrix bitMatrix = new MultiFormatWriter().encode(contents, BarcodeFormat.QR_CODE, WIDTH, HEIGHT, hints);
+            MatrixToImageWriter.writeToStream(bitMatrix, FORMAT,out);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     *
+     * @Title:getQRresult
+     * @Description:读取二维码
+     * @param filePath
+     * @return
+     * @author doubledumbao
+     * @修改时间：2018年2月26日 上午9:45:19
+     * @修改内容：创建
+     */
+    public static Result getQRresult(String filePath) {
+        try {
+            return  getQRresult(ImageIO.read(new File(filePath)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Result getQRresult(InputStream input) {
+        try {
+            return  getQRresult(ImageIO.read(input));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Result getQRresult(BufferedImage bufferedImage) {
+        /**
+         * 如果用的jdk是1.9，需要配置下面这一行。
+         */
+        //System.setProperty("java.specification.version", "1.9");
+        Result result = null;
+        try {
+            BinaryBitmap bitmap = new BinaryBitmap(
+                    new HybridBinarizer(new BufferedImageLuminanceSource(bufferedImage)));
+
+            HashMap hints = new HashMap<>();
+            hints.put(EncodeHintType.CHARACTER_SET, CHARTSET);
+            result = new MultiFormatReader().decode(bitmap, hints);
+        } catch (NotFoundException e) {
+            result = getQRresultQRCode(bufferedImage);
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    /**
+     * 剪切图片后再处理
+     * @return
+     */
+    public static Result getQRresultQRCode(BufferedImage bufferedImage) {
+        Result result = null;
+        try {
+            //QRCode解码器
+            QRCodeDecoder codeDecoder = new QRCodeDecoder();
+            /**
+             *codeDecoder.decode(new MyQRCodeImage())
+             *这里需要实现QRCodeImage接口，移步最后一段代码
+             */
+            //通过解析二维码获得信息
+            String _result = new String(codeDecoder.decode(new MyQRCodeImage(bufferedImage)), "utf-8");
+            if(_result!=null){
+                result = new Result(_result,null,null,null);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static Result getQRresultByWeb(byte[] imgbyte) {
+        Result result = null;
+        try {
+            MyOkHttp req = new MyOkHttp();
+            Map files = new HashMap<String,byte[]>();
+            files.put("file",imgbyte);
+            HttpReturn ret = req.PostImage("https://www.sojson.com/qrcode/deqrByImages.shtml",null,files);
+            if(ret.code==200){
+                JSONObject json = new JSONObject(ret.body);
+                result = new Result(json.getString("txt"),null,null,null);
+            }
+            //System.out.println(retstr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+
+
+    private static class MyQRCodeImage implements QRCodeImage {
+        BufferedImage bufferedImage;
+
+        public MyQRCodeImage(BufferedImage bufferedImage){
+            this.bufferedImage=bufferedImage;
+        }
+
+        //宽
+        @Override
+        public int getWidth() {
+            return bufferedImage.getWidth();
+        }
+
+        //高
+        @Override
+        public int getHeight() {
+            return bufferedImage.getHeight();
+        }
+
+        //像素还是颜色
+        @Override
+        public int getPixel(int i, int j) {
+            return bufferedImage.getRGB(i,j);
+        }
+    }
+
+}
